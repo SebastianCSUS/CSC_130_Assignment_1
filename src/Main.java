d6ef8413dd28dd337bc0b790ce225f96992c0f49
Sebastian-ABJ <88410272+Sebastian-ABJ@users.noreply.github.com>
2021-09-21 12:54:43 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/** Assignment 1 - Proper Stack and Queue - 9/28/2021
+ *  Sebastian Jones - Sacramento State University - CSC 130 - Professor Cooke
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello world!");
+    }
+}
+
+/** Node Class
+ *
+ */
+class Node {
+    public Object Value;
+    public Node Next;
+}
